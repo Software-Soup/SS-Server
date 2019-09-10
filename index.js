b4288c2b688a929ce1374df0580dbd415470717f
Daquaris Chadwick <sourhead5@gmail.com>
2019-09-09 22:20:33 -0400

    added basic webpage and server code

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/wwwsoupnet');
+app.use(express.static(__dirname + '/public'));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/wwwsoupnet', {useNewUrlParser: true});
 var db = mongoose.connection;
 
-db.on('error',
-	function()
-	{
-		console.log('connection error');
-	}
-);
-db.once('open', 
-	function()
-	{
-		console.log('mongo working!');
-	}
-);
-
-app.get('/ping', 
-    function(req, res) 
+app.get('/',
+    function(req, res)
     {
-        res.send({ping: 'hello this is server and i am alive'});
+        res.render('index', {title: 'Software Soup Home'});
     }
 );
 
-app.get('/ping/:id', 
-    function(req, res) 
-    {
-        res.send({ping: 'hello this is server and i am got '+req.param.id});
-    }
-);
-
-app.listen(3000, 
+app.listen(3000,
     function()
     {
         console.log('Listening on port 3000');
